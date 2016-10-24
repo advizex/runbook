a717bb944f28a1c5ad812adb3a3cde76b882738c
Daniel McSorley <mcsorley.1@gmail.com>
2016-10-24 15:40:04 -0400

    fix pillow dependencies

--- dockerit.sh
+++ dockerit.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -ex
 
 # add all our dependencies
-DEBS='python-setuptools make graphviz'
+DEBS='python-dev python-setuptools make graphviz zlib1g-dev libjpeg62-turbo-dev gcc'
 apt-get update
 apt-get install -y $DEBS
 easy_install pip
+pip install --upgrade pip
 pip install --no-cache-dir -r /projects/project/requirements.txt
 
 # build the content and put it where nginx expects
